50d57e9b98204ed013b7bfad9c2dcd1c5b093c64
OhJino <47510297+OhJino@users.noreply.github.com>
2019-02-27 01:31:27 -0800

    Update designs.js

--- designs.js
+++ designs.js
@@ -1,23 +1,24 @@
 var size = document.getElementById("sizePicker");
 size.addEventListener("submit", function(event) {
-    event.preventDefault();
-    makeGrid();
+    event.preventDefault();     //Prevents page from being refreshed
+    makeGrid();     //Make grid when submit button is clicked
 });
 
 function makeGrid() {
     var table = document.getElementById("pixelCanvas");
+    table.innerHTML = "";   //Table starts off as a blank canvas
     var height = document.getElementById("inputHeight").value;
     var width = document.getElementById("inputWidth").value;
     for (var x = 0; x < height; x++) {
-        var newRow = document.createElement('tr');
+        var newRow = document.createElement('tr');      //Create new row
         for (var y = 0; y < width; y++) {
-            var newCell = document.createElement('td');
+            var newCell = document.createElement('td');     //Create new columns for each row
             newRow.appendChild(newCell);
-            newCell.addEventListener('click', function(event) {
+            newCell.addEventListener('click', function(event) {     //Change color of cell when clickd on
                 var color = document.getElementById("colorPicker").value;
                 event.target.style.backgroundColor = color;
             })
         }
-    table.appendChild(newRow);
+    table.appendChild(newRow);      //Displays rows and columns in browser
     }
 };
